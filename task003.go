bd55a6039bbea134406a433d80227d5cf5f547fb
Aleksey <lilo@live.ru>
2020-07-22 11:33:59 +0300

    Test

--- task003.go
+++ task003.go
@@ -12,7 +12,12 @@ func main() {
 
 	fmt.Fscan(os.Stdin, &A)
 	fmt.Fscan(os.Stdin, &B)
+<<<<<<< Updated upstream
 	resulr := (A + B)
 	s := resulr * resulr
+=======
+	resulr := (A * *B)
+	//s := resulr * B
+>>>>>>> Stashed changes
 	fmt.Println(s)
 }
